daaec2d172a3f7e1d7802862e9b110dcdc225971
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2024-03-04 10:28:49 -0300

    test: deve ser possivel criar uma fatura

--- gerador-de-nota-fiscal/src/test/java/com/example/FaturaTest.java
+++ gerador-de-nota-fiscal/src/test/java/com/example/FaturaTest.java
@@ -1,5 +1,16 @@
 package com.example;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Date;
+import org.junit.Test;
+
 public class FaturaTest {
-    
+    @Test
+    public void instanciarFatura() {
+        Fatura fatura = new Fatura("Cliente Teste", "Endereço Teste", "CONSULTORIA", 1000.0);
+    }
+
+   
 }
