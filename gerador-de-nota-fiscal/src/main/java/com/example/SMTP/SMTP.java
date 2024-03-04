05c20731240ce0bec42404d60d4fdffe52c42a7d
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2024-03-04 13:47:45 -0300

    feat: creates envia metodo

--- gerador-de-nota-fiscal/src/main/java/com/example/SMTP/SMTP.java
+++ gerador-de-nota-fiscal/src/main/java/com/example/SMTP/SMTP.java
@@ -3,6 +3,8 @@ package com.example.SMTP;
 import com.example.NotaFiscal.NotaFiscal;
 
 public class SMTP {
-	
+	public void envia(NotaFiscal nf) { 
+		System.out.println("Enviando por e-mail"); 
+	}
 }
 
