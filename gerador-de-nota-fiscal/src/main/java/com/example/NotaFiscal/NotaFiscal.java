745e16017b688e641a3de5e607e9eb46795dd7bf
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2024-03-04 11:45:27 -0300

    feat: implementa getNomeCLiente

--- gerador-de-nota-fiscal/src/main/java/com/example/NotaFiscal/NotaFiscal.java
+++ gerador-de-nota-fiscal/src/main/java/com/example/NotaFiscal/NotaFiscal.java
@@ -10,4 +10,8 @@ public class NotaFiscal {
         this.valor = valor;
         this.valorImposto = valorImposto;
     }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
 }
