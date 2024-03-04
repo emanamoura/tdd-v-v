e6c525599a244725771d1dacbd68912c6924027e
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2024-03-04 11:06:09 -0300

    feat: cria classe Fatura

---
+++ gerador-de-nota-fiscal/src/main/java/com/example/Fatura.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Fatura {
+    private String nomeCliente;
+    private String enderecoCliente;
+    private String tipoServico;
+    private double valor;
+
+    public Fatura(String nomeCliente, String enderecoCliente, String tipoServico, double valor) {
+        this.nomeCliente = nomeCliente;
+        this.enderecoCliente = enderecoCliente;
+        this.tipoServico = tipoServico;
+        this.valor = valor;
+    }
+}
