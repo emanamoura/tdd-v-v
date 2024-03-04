bd40a2c6b4d7930d98acb8a30be5437737951b7e
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2024-03-04 12:11:42 -0300

    feat: implementa envia SAP

--- gerador-de-nota-fiscal/src/main/java/com/example/SAP/SAP.java
+++ gerador-de-nota-fiscal/src/main/java/com/example/SAP/SAP.java
@@ -4,6 +4,6 @@ import com.example.NotaFiscal.NotaFiscal;
 
 public class SAP {
     public void envia(NotaFiscal nf) {
-        System.out.println("Nota fiscal enviada para o SAP");
+        System.out.println("Enviando pro SAP");
     }
 }
